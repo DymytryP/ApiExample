ea8168074a271844c55a918ecab34cf689037226
dmitrijs.pupkovs <dmitrijs.pupkovs@gmail.com>
2021-10-16 11:58:12 +0300

    Add validation to base post controller class Make base post controller class abstract Make small code adjustments and fix descriptions

--- Billing.Api/Controllers/PostController.cs
+++ Billing.Api/Controllers/PostController.cs
@@ -1,29 +1,37 @@
 ﻿using Billing.Infrastructure.Contracts;
+using Billing.Infrastructure.Contracts.Models;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Billing.Api.Controllers
 {
-    public class PostController<TRequestData> : ControllerBase
+    public class PostController<TRequestData> : ControllerBase where TRequestData : IRequestData
     {
-        private readonly IDataRequestPipeline<TRequestData> requestPipeline;
+        private readonly IRequestPipeline<TRequestData> requestPipeline;
 
-        public PostController(IDataRequestPipeline<TRequestData> requestPipeline)
+        private readonly AbstractValidator<TRequestData> requestValidationRules;
+
+        public PostController(
+            IRequestPipeline<TRequestData> requestPipeline,
+            AbstractValidator<TRequestData> requestValidationRules)
         {
             this.requestPipeline = requestPipeline;
+            this.requestValidationRules = requestValidationRules;
         }
 
         /// <summary>
         /// Processes request.
         /// </summary>
         /// <param name="requestData">Request data object.</param>
-        /// <returns>JSON </returns>
+        /// <returns>Result object as JSON</returns>
         [Route("{action}")]
         [HttpPost]
         public async Task<IActionResult> Process(TRequestData requestData)
         {
+            await this.requestValidationRules.ValidateAndThrowAsync(requestData);
 
-            var result = await requestPipeline.ProcessRequestAsync(requestData);
+            var result = await this.requestPipeline.ProcessRequestAsync(requestData);
 
             return Ok(result);
         }
