55b3c3465043cc8081fb05ba1df85a05c54cde56
dmitrijs.pupkovs <dmitrijs.pupkovs@gmail.com>
2021-10-18 16:44:15 +0300

    Add order request validator

---
+++ Billing.Infrastructure/Validators/Orders/OrderRequestDataValidator.cs
@@ -0,0 +1,34 @@
+﻿using Billing.Infrastructure.Models.Enums;
+using Billing.Infrastructure.Models.Orders.RequestData;
+using FluentValidation;
+using System;
+using System.Linq;
+
+namespace Billing.Infrastructure.Validators.Orders
+{
+    public class OrderRequestDataValidator : AbstractValidator<OrderRequestData>
+    {
+        public OrderRequestDataValidator()
+        {
+            decimal minimumAmount = 0.01m;
+            decimal maximumAmount = 1000000.00m;
+            RuleFor(ord => ord.PayableAmount.Amount)
+                .Cascade(CascadeMode.Stop)
+                .Must(amount => amount >= minimumAmount)
+                .Must(amount => amount <= maximumAmount)
+                .WithMessage($"Amount must be between {minimumAmount} and {maximumAmount}.");
+
+            int minimumQuantity = 1;
+            int maximumQuantity = 100;
+            RuleFor(ord => ord.CartItems)
+                .Must(cartItems => cartItems
+                    .All(item =>
+                        item.Quantity >= minimumQuantity
+                        && item.Quantity <= maximumQuantity));
+
+            RuleFor(ord => ord.PaymentGateway)
+                .Must(paymentGateway => Enum.GetNames<PaymentGateway>()
+                    .Any(name => name == paymentGateway));
+        }
+    }
+}
